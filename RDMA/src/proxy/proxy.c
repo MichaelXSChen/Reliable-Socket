7c90ef965036f656427647ddcf128aeba1f5c0b0
Michael Xusheng Chen <michael.xschen@gmail.com>
2017-01-17 02:34:32 +0800

    debug msg

--- RDMA/src/proxy/proxy.c
+++ RDMA/src/proxy/proxy.c
@@ -151,6 +151,7 @@ int dare_main(proxy_node* proxy, const char* config_path)
     pthread_t thread;
     pthread_create(&thread, NULL, handle_tcp_buffer, NULL); 
 
+    debugf("Consensus Module Init Completed");
 
     return 0;
 }
