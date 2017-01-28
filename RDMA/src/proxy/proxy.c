5274fbc6a08dcb0a3ea44580013e7cbc48ab8f0d
Michael Xusheng Chen <michael.xschen@gmail.com>
2017-01-28 22:00:27 +0800

    add wait for 0

--- RDMA/src/proxy/proxy.c
+++ RDMA/src/proxy/proxy.c
@@ -71,8 +71,8 @@ void *handle_tcp_buffer(void *useless){
                 pthread_mutex_lock(&tcp_no_empty_lock);
                 pthread_cond_wait(&tcp_no_empty, &tcp_no_empty_lock);
                 pthread_mutex_unlock(&tcp_no_empty_lock);
-            }else if (ret == -1){
-
+            }
+            
         #elif defined(MUTEX_LOCK)
             LOCK(out_lock);
             if (ret == 0){
